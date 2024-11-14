e5f49dcc1af2a82f4ebe3a538e62ce7f588f64f3
tanishq <tanishqjain23112001@gmail.com>
2024-11-15 04:08:39 +0530

    route handlers/ middlewares

--- src/app.js
+++ src/app.js
@@ -4,20 +4,26 @@ const express =  require("express");
 
 const app = express(); 
 
-
-// yaha per koi bhi request jari hai server per , to server uske reesponse mai  "Hello from the server"  bhej dera hai.
-app.use("/user" , (req,res)=>{
-    res.send("Hhaahahahaahaha");
-});
-
-app.get("/user" , (req,res)=>{
-  res.send({firstName:"tanishq" , lastName:"jain"});
-});
-
-app.post("/user", (req,res)=>{
-    res.send( "data successfully sned to database");
-})
-
+//creating routes
+app.use("/user" , 
+    //first route handler
+    (req,res ,next)=>{
+    console.log("Handling the route user 1")
+    // res.send("Route handler 1");
+    next();
+},  
+    //second route handler
+    (req,res,next)=>{
+    console.log("Handling the route user 2")
+    // res.send("Route handler 2");
+    next();
+},  
+    //third route handler
+    (req,res)=>{
+    console.log("Handling the route user 3")
+    res.send("Route handler 3");
+}
+)
 
 //server is listning on PORT 3000=>
 app.listen(3000 , ()=>{
