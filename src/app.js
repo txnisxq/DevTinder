574e4d554f2ea4f6cac49f6418e1f98da825a06c
tanishq <tanishqjain23112001@gmail.com>
2024-11-18 04:32:34 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const User = require("./models/user");
 app.use(express.json());
 
 
-
+//this is cretaing post api: for signIn purpose:
 app.post("/signup" , async (req,res)=>{
      
     //creating a new instance of a User model=>
@@ -28,6 +28,97 @@ app.post("/signup" , async (req,res)=>{
    
 
 });
+
+
+
+//getting useer by email:
+app.get("/user" , async (req , res)=>{
+
+    const userEmail = req.body.emailId;
+
+    try{
+        // console.log(userEmail);
+       const user = await User.findOne({emailId:userEmail});
+       if(!user){
+        res.send("User not found");
+       }
+       else{
+        res.send(user);
+       }
+       
+    }
+    catch(err){
+        res.status(400).send("something went wrong" + err.message);
+    }
+});
+ 
+
+
+//now i am creating a feed API : to get all the users from the database:
+app.get("/feed" , async(req,res)=>{
+    
+    try{
+        const users = await User.find({}); 
+        console.log(users);
+        if(!users){
+            res.send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("something went wrong" + err.message);
+    }
+
+});
+
+
+
+//delete a data from the data base
+app.delete("/deleteUser", async(req,res)=>{
+    
+    const userId = req.body._id;
+    try{
+        if(!userId){
+           res.send("user does not exist");
+        }
+        else{
+            // await User.findByIdAndDelete({userId});      u can also write "userId" only  insted of  "_id:userId" in curly brackets in below line.
+            await User.findByIdAndDelete({_id:userId});
+            res.send("userId deleted successfully");
+        }
+        
+    }
+    catch(err){
+         res.status(404).send("error while deleting user");
+    }
+    
+});
+
+
+
+
+//update data of a user 
+app.patch("/updateUser" , async(req,res)=>{
+    const userId = req.body._id;
+    const data = req.body;
+
+    try{
+        if(!userId){
+            res.send("user not found")
+        }
+        else{
+            //  u can also write "userId" only,  insted of  "_id:userId" in curly brackets in below line.
+            const user = await User.findByIdAndUpdate({_id:userId} , data , {runValidators:true});
+            res.send("user updated successfully");
+        }
+        
+    }
+    catch(err){
+        res.status(400).send("error while updating user");
+    }
+})
 
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,46 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
     },
     lastName:{
         type:String,
+        
     },
     emailId:{
         type:String,
+        required:true,    //required means it is mendatory to fill this field
+        unique:true,      //unique means that if u signup with same emailid that aready exits, then it won't let u signup;
+        lowercase:true,   //it convert the uppercase if exist in user emailId to lowercase.
+        trim:true,        //if user type its emailId with spaces between , it repair it.
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
-        type:Number,
+        type:Number, 
     },
     gender:{
         type:String,
+        enum:["male" , "female", "others"],
+    },
+    photoUrl:{
+        type:String,
+        default:"https://img.freepik.com/free-vector/blank-user-circles_78370-4336.jpg",
+    },
+    about:{
+        type:String,
+        default:"This is default info about user",  // this about section can take default value in case if user dont put anything inside it by their own.
+    },
+    skills:{
+        type:[String],  //hear i am writing string insside square brackets because , we can write multiple skills , so this square bracket is an array which can take multiple string values.
     }
-});
+},
+{
+    timestamps:true,
+}
+
+);
 
 module.exports = mongoose.model("User" , userSchema);
