4ab5630f33de895512ada7923ded2822a88b5e9b
Tanishq Jain <tanishq@gmail.com>
2026-03-26 16:58:34 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 // }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://devlify.co.in"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // ✅ add PATCH
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
